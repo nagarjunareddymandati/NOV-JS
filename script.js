b2fab058f0df53034a333f53c5464a8dbdaf6f1b
MANDATI NAGARJUNA REDDY <97222419+nagarjunareddymandati@users.noreply.github.com>
2024-01-15 12:36:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
-// Write a function that takes two numbers (a and b) as argument
-// Return b percent of a
-
-
-
 function myFunction(a, b) {
    
-    return a * b / 100;
-}
+    return a === b;
+ }
 
 
 
-
-
-
-
-// myFunction(100,50)
-// Expected
-// 50
-// myFunction(10,1)
-// Expected
-// 0.1
-// myFunction(500,25)
-// Expected
-// 125
+// myFunction(2, 3)              //  Expected                 false 
+// myFunction(3, 3)                Expected                 true 
+// myFunction(1, '1')              Expected                 false 
+// myFunction('10', '10')          Expected                 true 
